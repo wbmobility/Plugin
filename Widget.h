86955cdaf389577ff64935f9eaeac46cc24f030e
wbmobility <wbmobility@gmail.com>
2013-09-05 13:33:19 +0530

    Adding Files

---
+++ Widget.h
@@ -0,0 +1,13 @@
+//
+//  Widget.h
+//  Serenity
+//
+//  Created by Admin on 29/08/13.
+//  Copyright (c) 2013 WB. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Widget : NSObject
+
+@end
---
+++ Widget.m
@@ -0,0 +1,13 @@
+//
+//  Widget.m
+//  Serenity
+//
+//  Created by Admin on 29/08/13.
+//  Copyright (c) 2013 WB. All rights reserved.
+//
+
+#import "Widget.h"
+
+@implementation Widget
+
+@end
